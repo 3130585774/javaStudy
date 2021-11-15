46f49437e559802d95f16c58d45b09544c1d9b88
Rusrty <3130585774@qq.com>
2021-11-15 15:03:35 +0800

    123

--- fcState.java
+++ fcState.java
@@ -151,6 +151,21 @@ public class fcState {
 				a++;
 			}
 			System.out.println("while ----" + sum2);
+			
+			//202111151410
+			iTemp = (int) (Math.random() * 20 + 1);
+			int iN = iTemp;
+			int i = 1;
+			System.out.println(iN);
+			while (i <= iN) {
+            for (int j = 0; j < iN - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print((char) (i + 64));
+            }
+            System.out.println("");
+            i++;
 
 		} catch (Exception e) {
 			System.out.println("Error!");
