a4b174b2f52beb02c402f9b1b36ae76bf64168fa
Rusrty <3130585774@qq.com>
2021-11-15 14:59:47 +0800

    213

--- test.java
+++ test.java
@@ -1,19 +1,19 @@
 public class test {
     public static void main(String[] args) {
-    int iTemp;
-    iTemp = (int)( Math.random()*20+1);
-    int iN = iTemp;
-    int i = 1;
-    System.out.println(iN);
-    while(i<=iN){
-        for(int j = 0;j<iN-i;j++){
-            System.out.print(" ");
+        int iTemp;
+        iTemp = (int) (Math.random() * 20 + 1);
+        int iN = iTemp;
+        int i = 1;
+        System.out.println(iN);
+        while (i <= iN) {
+            for (int j = 0; j < iN - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print((char) (i + 64));
+            }
+            System.out.println("");
+            i++;
         }
-        for(int j = 0;j<2*i-1;j++){
-            System.out.print((char)(i + 64));
-        }
-        System.out.println("");
-        i++;
-    }
     }
 }
