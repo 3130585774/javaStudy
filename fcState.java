3516420c6c6eaa46bd8eee277c51e44da5955b73
Rusrty <3130585774@qq.com>
2021-11-15 17:50:18 +0800

    t

--- fcState.java
+++ fcState.java
@@ -159,6 +159,7 @@ public class fcState {
 				}
 				for (int j = 0; j < 2 * i - 1; j++) {
 					System.out.print((char) (i + 64));
+					System.out.println("*");
 				}
 				System.out.println("");
 				i++;
