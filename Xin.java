7565a5b5604d2dcecb40af5b3bd2de021b3ce9a7
Rusrty <3130585774@qq.com>
2021-11-24 13:40:46 +0800

    123

--- Xin.java
+++ Xin.java
@@ -1,21 +1,16 @@
 public class Xin {
-    public static void main(String argv[]){
-        for(float y = (float) 1.5;y>-1.5;y -=0.1)
-        {
-               for(float x= (float) -1.5;x<1.5;x+= 0.038)
-               {
-                     float a = x*x+y*y-1;
-                     if((a*a*a-x*x*y*y*y)<=0.0)
-                     {
-                            
-                            
-                            System.out.print("*");
-                     
-                     }
-                     else
-                            System.out.print(" ");
-               }
-               System.out.print("\n");
+    public static void main(String argv[]) {
+        for (float y = (float) 1.5; y > -1.5; y -= 0.1) {
+            for (float x = (float) -1.5; x < 1.5; x += 0.038) {
+                float a = x * x + y * y - 1;
+                if ((a * a * a - x * x * y * y * y) <= 0.0) {
+
+                    System.out.print("*");
+
+                } else
+                    System.out.print(" ");
+            }
+            System.out.print("\n");
         }
- }
+    }
 }
