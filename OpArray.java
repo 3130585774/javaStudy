1911ce483d56ce12e2182fda07dcd907441fa210
Rusrty <3130585774@qq.com>
2021-11-29 15:00:08 +0800

    123123

--- OpArray.java
+++ OpArray.java
@@ -1,37 +1,57 @@
-public class OpArray{
-	//main: ontrance of function
+public class OpArray {
+	// main: ontrance of function
 	static final int SZARRAY = 16;
-	public static void main(String[]args){
-		
-		  try{
-		   //20211122
-		   //byte short int long float double char boolean
-		   //array
-		     
-		     char cTemp = 'a';
-	 	     int iTemp = cTemp;
-		     System.out.println( "iTemp = "+ iTemp );
 
-		   //defintion of array
-		   
- 		     //size of array 2 n
-		     //size is not real num
+	public static void main(String[] args) {
 
-		     char[] arrTemp = new char[SZARRAY];
+		try {
+			// 20211122
+			// byte short int long float double char boolean
+			// array
 
-		     for(int i = 0; i < SZARRAY; i++){
-			     arrTemp[i] = (char)(cTemp + i);
-		     }
-		     System.out.println( "arrTemp = " + arrTemp );
+			char cTemp = 'a';
+			int iTemp = cTemp;
+			System.out.println("iTemp = " + iTemp);
 
-		     for(int i = 0; i < SZARRAY ; i++){
-		     
-			     System.out.print(arrTemp[i]+" ");
-			     }
-		     }catch (Exception e) {
-		     
-		     
-		    }
-	}		 
-   
+			// defintion of array
+
+			// size of array 2 n
+			// size is not real num
+
+			char[] arrTemp = new char[SZARRAY];
+
+			for (int i = 0; i < SZARRAY; i++) {
+				arrTemp[i] = (char) (cTemp + i);
+			}
+			System.out.println("arrTemp = " + arrTemp);
+
+			for (int i = 0; i < SZARRAY; i++) {
+
+				System.out.print(arrTemp[i] + " ");
+			}
+			System.out.println("");
+
+			// 20211129 1419
+			int[] iarr = new int[SZARRAY];
+			for (int i = 1; i < SZARRAY; i++) {
+				iarr[i] = (int) (Math.random() * 100 + 1);
+			}
+			int iFound = (int) (Math.random() * 100 + 1);
+			iarr[0] = iFound;
+			for (int i = 0; i < SZARRAY; i++)
+			{
+				System.out.println(iarr[i] + "   ");
+			}
+			System.out.println("");
+			for (int i = 1; i < SZARRAY; i++) {
+				if (iarr[0] == iarr[i]) {
+					System.out.println(iFound + " is found");
+				}
+			}
+			System.out.println("Can found");
+		} catch (Exception e) {
+
+		}
+	}
+
 }
