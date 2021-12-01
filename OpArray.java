213e12fd5cf883ce92f299bf73920b2d2801b5e8
Rusrty <3130585774@qq.com>
2021-12-01 18:29:25 +0800

    Giao

--- OpArray.java
+++ OpArray.java
@@ -1,6 +1,6 @@
 public class OpArray {
 	// main: ontrance of function
-	static final int SZARRAY = 1000;
+	static final int SZARRAY = 16;
 
 	public static void main(String[] args) {
 
@@ -31,11 +31,13 @@ public class OpArray {
 			System.out.println("");
 
 			// 20211129 1419
+			int iarrsum = 0;
 			int[] iarr = new int[SZARRAY];
 
 
 			for (int i = 1; i < SZARRAY; i++) {
 				iarr[i] = (int) (Math.random() * 100 + 1);
+				iarrsum += iarr[i];
 			}
 
 			int iFound = (int) (Math.random() * 100 + 1);
@@ -44,18 +46,30 @@ public class OpArray {
 
 			for (int i = 0; i < SZARRAY; i++)
 			{
-				System.out.println(iarr[i] + "   iarr["+i+"]");
+				System.out.println(iarr[i]);
 			}
 			System.out.println("");
 			
+			boolean bF = false;
+			int con = 0;
+			int bigthan = 0;
 			for (int i = 1; i < SZARRAY; i++) {
 				if (iarr[0] == iarr[i]) {
 					System.out.println(iFound + " is found");
-					System.exit(0); //退出程序
+					bF = true;
+					con += 1;
+				}
+				if(iarr[0] < iarr[i]){
+					bigthan += 1;
 				}
 			}
-
+			if (bF == false){
 			System.out.println(iFound + " not found");
+			}
+			float adv = (float)iarrsum / SZARRAY;
+			System.out.println("iFound appear "+con);
+			System.out.println("bigger than iFound have"+bigthan);
+			System.out.println("advtage = "+adv);
 
 		} catch (Exception e) {
 
